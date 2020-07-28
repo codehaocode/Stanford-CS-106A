8544ee6a6f24296c9c80489d18509f4dfc8e9826
codehaocode <yuhao.zhong@code.berlin>
2020-07-28 17:02:19 +0200

    solve assignment 2 problem 3 Hailstone

--- 2/CS106A-Assignment2/src/Hailstone.java
+++ 2/CS106A-Assignment2/src/Hailstone.java
@@ -4,6 +4,38 @@ import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
 	public void run() {
-		// TODO: finish this program	
+		// TODO: finish this program
+		println("This program computes Hailstone sequences.");
+		
+		int num = readInt("Enter a number: ");
+		
+		computeHailstone(num);
+		
+	}
+	
+	private void computeHailstone(int n) {
+		
+		int steps = 0;
+		
+		while (true) {
+			if (n == 1) {
+				break;
+			} else if ((n % 2 == 1)) {
+				int m = 3 * n + 1;
+				println(n + " is odd, so I make 3n  + 1: " + m);
+				n = m;
+			} else {
+				int m = n / 2;
+				println(n + " is even, so I take half: " + m);
+				n = m;
+			}
+			
+		steps++;
+		}
+		println("It took " + steps + " steps to reach 1.");
+		if (readBoolean("Run again? ", "y", "n")) {
+			int num = readInt("Enter a number: ");	
+			computeHailstone(num);
+		}
 	}
 }
