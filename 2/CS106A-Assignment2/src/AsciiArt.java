7dba4d6787d63564e256e922f043684d46676aff
codehaocode <yuhao.zhong@code.berlin>
2020-08-13 11:26:12 +0200

    Finish Assignment 2 Ascii Art

--- 2/CS106A-Assignment2/src/AsciiArt.java
+++ 2/CS106A-Assignment2/src/AsciiArt.java
@@ -1,7 +1,24 @@
+import java.awt.Color;
+
 import acm.program.*;
 
 public class AsciiArt extends ConsoleProgram {
 	public void run() {
 		// TODO: finish this program
-	}
+		println("CS 106A ASCII Art by Yuhao Zhong", Color.RED);
+		
+		for (int i = 1; i < 10; i++) {
+				for (int j = 10; j > i; j--) {
+					print(" ");
+				}
+				for (int r = i; r > 0; r--) {
+					print("/", Color.CYAN);
+				}
+				for (int q = i; q > 0; q--) {
+					print("\\", Color.GREEN);
+				}
+				println("");
+			}
+		}
+	
 }
